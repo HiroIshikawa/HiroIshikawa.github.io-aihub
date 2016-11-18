f3f0f25287d08376b2605b4df2faa04e520512b5
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-18 01:44:46 -0800

    Fixed project page routings

--- index.jsx
+++ index.jsx
@@ -11,14 +11,14 @@ import ExampleTwoDeepComponent from './components/ExampleTwoDeepComponent';
 
 
 const routes = (
-  <Route path="/" mapMenuTitle="Home" component={App}>
+  <Route path="/HiroIshikawa.github.io-aihub/" mapMenuTitle="Home" component={App}>
     <IndexRoute component={Home} />
 
-    <Route path="example" mapMenuTitle="Example" component={ExampleComponent}>
-      <Route path="two-deep" mapMenuTitle="Two Deep" component={ExampleTwoDeepComponent} />
+    <Route path="/HiroIshikawa.github.io-aihub/example" mapMenuTitle="Example" component={ExampleComponent}>
+      <Route path="/HiroIshikawa.github.io-aihub/two-deep" mapMenuTitle="Two Deep" component={ExampleTwoDeepComponent} />
     </Route>
 
-    <Route path="*" mapMenuTitle="Page Not Found" component={PageNotFound} />
+    <Route path="/HiroIshikawa.github.io-aihub/*" mapMenuTitle="Page Not Found" component={PageNotFound} />
   </Route>
 );
 
