ca955016c4fa03992775d06ef99258c697295f29
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-23 01:29:07 -0800

    Add partition in navigation

--- components/App.jsx
+++ components/App.jsx
@@ -36,11 +36,11 @@ function App({ children, routes }) {
       <h1>aihub</h1>
       <a href={repoLink}>https://github.com/HiroIshikawa/HiroIshikawa.github.io-aihub</a>
       <div>
-        <Link to="/HiroIshikawa.github.io-aihub/">Home   </Link>
-        <Link to="/HiroIshikawa.github.io-aihub/about">About   </Link>
-        <Link to="/HiroIshikawa.github.io-aihub/project">Project   </Link>
-        <Link to="/HiroIshikawa.github.io-aihub/books">Books   </Link>
-        <Link to="/HiroIshikawa.github.io-aihub/blog">Blog   </Link>
+        <Link to="/HiroIshikawa.github.io-aihub/">Home |</Link>
+        <Link to="/HiroIshikawa.github.io-aihub/about">About |</Link>
+        <Link to="/HiroIshikawa.github.io-aihub/project">Project |</Link>
+        <Link to="/HiroIshikawa.github.io-aihub/books">Books |</Link>
+        <Link to="/HiroIshikawa.github.io-aihub/blog">Blog |</Link>
         <Link to="/HiroIshikawa.github.io-aihub/note">Note</Link>
       </div>
       {children}
