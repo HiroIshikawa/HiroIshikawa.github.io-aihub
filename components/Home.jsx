f1d07ce8cbf950ac231deb6d39129766f3f02276
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-23 01:23:02 -0800

    Delete duplicates from Home page

--- components/Home.jsx
+++ components/Home.jsx
@@ -6,11 +6,6 @@ function Home() {
 
   return (
     <div>
-      <div><Link to="/HiroIshikawa.github.io-aihub/about">About   </Link>
-      <Link to="/HiroIshikawa.github.io-aihub/project">Project   </Link>
-      <Link to="/HiroIshikawa.github.io-aihub/books">Books   </Link>
-      <Link to="/HiroIshikawa.github.io-aihub/blog">Blog   </Link>
-      <Link to="/HiroIshikawa.github.io-aihub/note">Note</Link></div>
       <p>
       To build thoughtful AI.
       </p>
