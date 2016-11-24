a70fd5a07929c14911e2e3e8a720088fe1a93974
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-24 00:04:17 -0800

    Add lines and spaces

--- components/Home.jsx
+++ components/Home.jsx
@@ -7,26 +7,28 @@ function Home() {
   return (
     <div>
       <header>
+        <br>
+        <br>
         <h1>Tak Ishikawa</h1>
         <p>
           I am a senior CS student at
           <a href="https://www.ics.uci.edu/" target="_blank">UC Irvine</a>
           , previously interned at Drivemode as 
-          <a href="https://drivemode.com/" target="_blank">website developer</a>
+          <a href="https://drivemode.com/" target="_blank"> website developer</a>
           , and as 
-          <a href="https://www.instagram.com/p/4m53p0vB7h/?taken-by=drivemodeapp" target="_blank">data engineer (a.k.a. data cowboy)</a>
+          <a href="https://www.instagram.com/p/4m53p0vB7h/?taken-by=drivemodeapp" target="_blank"> data engineer (a.k.a. data cowboy)</a>
           , worked on 
-          <a href="https://github.com/openMF/community-app/pull/1541" target="_blank">Mifos X opensource project</a>
+          <a href="https://github.com/openMF/community-app/pull/1541" target="_blank"> Mifos X opensource project</a>
           , built 
-          <a href="https://github.com/HiroIshikawa/21playground" target="_blank">bitcoin apps on 21 bitcoin platform</a>
+          <a href="https://github.com/HiroIshikawa/21playground" target="_blank"> bitcoin apps on 21 bitcoin platform</a>
           , helped implementing controls system of 
-          <a href="http://www.hyperxite.com/pod/" target="_blank">Hyperloop UCI pod</a>
+          <a href="http://www.hyperxite.com/pod/" target="_blank"> Hyperloop UCI pod</a>
           , currently am working on 
-          <a href="https://github.com/ucirobotarm/UCI_Robot_Arm" target="_blank">image processing for UCI Robot Arm project</a>
+          <a href="https://github.com/ucirobotarm/UCI_Robot_Arm" target="_blank"> image processing for UCI Robot Arm project</a>
           , maintaining 
-          <a href="http://www.hyperxite.com/" target="_blank">the website of Hyperloop UCI team</a>
+          <a href="http://www.hyperxite.com/" target="_blank"> the website of Hyperloop UCI team</a>
           , and pursing 
-          <a href="https://docs.google.com/document/d/1gTmQ2BlEUNaJBtbQn0ilKq-KfU64lcE5jRhmROP72ZU/edit?usp=sharing" target="_blank">Machine Learning Engineer Nanodegree program</a>
+          <a href="https://docs.google.com/document/d/1gTmQ2BlEUNaJBtbQn0ilKq-KfU64lcE5jRhmROP72ZU/edit?usp=sharing" target="_blank"> Machine Learning Engineer Nanodegree program</a>
           .
         </p>
       </header>
