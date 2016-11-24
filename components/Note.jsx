fdf99ddf80ff8cc5113f26d08720331c8b5f4434
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-24 03:43:19 -0800

    Shorten content update on Note 2

--- components/Note.jsx
+++ components/Note.jsx
@@ -17,11 +17,7 @@ function Note() {
       <p><span>ML The Most Fundamental Components:</span></p>
       <ul>
         <li><span>Given known data, machine builds a model to predict given unknown data in the future.</span></li>
-      </ul>
-      <ul>
         <li><span>Dataset: Data that machine feeds in and builds a model based on.</span></li>
-      </ul>
-      <ul>
         <li><span>Training Samples: selected data points from dataset to train and build a model. The part of rows in the dataset.</span></li>
         <li><span>Attributes: variables that has set of domain values. All the columns of datasets.</span></li>
         <li><span>Features: selected attributes to be learned and tested to build and verify a model. Part of the columns of datasets; correspond to the inputs of a model.</span></li>
@@ -33,26 +29,24 @@ function Note() {
       </ul>
       <p><span>Data Modeling:</span></p>
       <ul>
-        <li><span>Data Types: It's important to know the types of data since each data types need different ways to be processed. Original data may be converted to other types of data by modification. </span></li>
-        <ul>
-          <li><span>Numeric Data: a measurement or count.</span></li>
-          <ul>
-            <li><span>Discrete: whole numbers.</span></li>
-            <li><span>Continuous: any number within range.</span></li>
-          </ul>
-            <li><span>Categorical Data: represents characteristics.</span></li>
-          <ul>
-            <li><span>may have numeric values but no mathematical meaning behind it.</span></li>
-            <li><span>Ordinal Data is an ordered Categorical Data.</span></li>
-          </ul>
-            <li><span>Times-Series Data: collected via repeated measurements over time.</span></li>
-          </ul>
-            <li><span>Encoding: at the end of the process, computers process information in the form of bits. We want to give numeric form of inputs to computers so that they can easily deal with datasets and conduct tasks we asked for. Encoding converts data into another form and, in particular in the context of machine learning, into numeric data.</span></li>
-            <li><span>One-Hot Encoding: a simple encoding that convert any categorical data attributes in numeric form. id. { Easy, Medium, Hard } -> { 0, 1, 2 }</span></li>
-            <li><span>The Training Set Size and Accuracy: fundamentally, more data gives better machine learning modeling. </span></li>
+      <li><span>Data Types: It's important to know the types of data since each data types need different ways to be processed. Original data may be converted to other types of data by modification. </span></li>
+      <ul>
+      <li><span>Numeric Data: a measurement or count.</span></li>
+      <ul>
+      <li><span>Discrete: whole numbers.</span></li>
+      <li><span>Continuous: any number within range.</span></li>
       </ul>
-
-      
+      <li><span>Categorical Data: represents characteristics.</span></li>
+      <ul>
+      <li><span>may have numeric values but no mathematical meaning behind it.</span></li>
+      <li><span>Ordinal Data is an ordered Categorical Data.</span></li>
+      </ul>
+      <li><span>Times-Series Data: collected via repeated measurements over time.</span></li>
+      </ul>
+      <li><span>Encoding: at the end of the process, computers process information in the form of bits. We want to give numeric form of inputs to computers so that they can easily deal with datasets and conduct tasks we asked for. Encoding converts data into another form and, in particular in the context of machine learning, into numeric data.</span></li>
+      <li><span>One-Hot Encoding: a simple encoding that convert any categorical data attributes in numeric form. id. { Easy, Medium, Hard } -&gt; { 0, 1, 2 }</span></li>
+      <li><span>The Training Set Size and Accuracy: fundamentally, more data gives better machine learning modeling.</span></li>
+      </ul>
     </div>
   );
 }
