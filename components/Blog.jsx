1381bf22001d9dcb02408c975f18919dd496b24c
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-25 01:41:20 -0800

    Modified blog content spacing

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -8,36 +8,36 @@ function Blog() {
   return (
     <div>
     <p>I put the most recent blog entry here. I put archives on Medium.</p>
-    <h2>Depth</h2>
+    <h1>Depth</h1>
       <p>
-      	We move around, dig shallow, and find nothing valualbe.
-      	From no matter where, when digging deeply enough, you'll find something valuable.
-      	Valuable things are huge enough to be found from many directions.
-      	It is the core. It locates at the very deep spot.
-      	Keep digging.
-      	No matter what happens, keep digging. 
-      	Don't give excuses to stop. 
-      	No matter what people say, keep digging. 
-      	Don't make any moment to argue.
+      	We move around, dig shallow, and find nothing valualbe. <br></br>
+      	From no matter where, when digging deeply enough, you'll find something valuable. <br></br>
+      	Valuable things are huge enough to be found from many directions. <br></br>
+      	It is the core. It locates at the very deep spot. <br></br>
+      	Keep digging. <br></br>
+      	No matter what happens, keep digging. <br></br>
+      	Don't give excuses to stop. <br></br>
+      	No matter what people say, keep digging. <br></br>
+      	Don't make any moment to argue. <br></br>
       	Keep digging.
       </p>
-    <h2>Choice</h2>
+    <h1>Choice</h1>
       <p>
-        The more choices we have, the happier we feel.
-        How can we have more choices? 
-        Stop reducing choices.
-        I mean, we tend to narrow our chioces by the internal internal voice. 
-        The voice in our head tells "No, you cannot do that." 
-        This is the real enemy. 
+        The more choices we have, the happier we feel. <br></br>
+        How can we have more choices? <br></br>
+        Stop reducing choices. <br></br>
+        I mean, we tend to narrow our chioces by the internal internal voice. <br></br>
+        The voice in our head tells "No, you cannot do that." <br></br>
+        This is the real enemy. <br></br>
         You realizes more choices if you liberate your heart from the noise.
       </p>
-    <h2>Meditation</h2>
+    <h1>Meditation</h1>
       <p>
-        Listen to the silence. 
-        Be aware of the sound of nothingness. 
-        Look at the air. 
-        Be aware of the color of emptiness. 
-        Travel here. 
+        Listen to the silence. <br></br>
+        Be aware of the sound of nothingness. <br></br>
+        Look at the air. <br></br>
+        Be aware of the color of emptiness. <br></br>
+        Travel here. <br></br>
         Be the present.
       </p>
     </div>
