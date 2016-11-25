b526718aa51176b8498cb2e0a09f6b4bff925f09
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-25 01:33:49 -0800

    Modified blog content

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -10,35 +10,35 @@ function Blog() {
     <p>I put the most recent blog entry here. I put archives on Medium.</p>
     <h2>Depth</h2>
       <p>
-      	We tend to move around, dig shallow, and find nothing valualbe.
-      	From no matter where, when digging deeply enough, you'll find valuable thing.
-      	Truly valuable things are huge enough to be found from many directions.
+      	We move around, dig shallow, and find nothing valualbe.
+      	From no matter where, when digging deeply enough, you'll find something valuable.
+      	Valuable things are huge enough to be found from many directions.
       	It is the core. It locates at the very deep spot.
-      	<br></br>
-      	<br></br>
-      	Why do we give up to dig deeply enough?
-      	Because of fear of missing out.
-      	How can we eliminate the fear of missing out?
-      	Just keep digging. Just keep digging.
-      	No matter what happens, just keep digging. 
-      	Who is the most often causes the event that precents you from keep doing? 
-      	Yourself. 
-      	Do not give excuse to yourself for stop doing it. 
-      	No matter what people say, just keep digging. 
-      	Who is the most often tell you stop doing it? 
-      	Youself. 
-      	Do not give the moment to yourself for arguing.
-      	Just keep digging.
+      	Keep digging.
+      	No matter what happens, keep digging. 
+      	Don't give excuses to stop. 
+      	No matter what people say, keep digging. 
+      	Don't make any moment to argue.
+      	Keep digging.
       </p>
     <h2>Choice</h2>
       <p>
         The more choices we have, the happier we feel.
         How can we have more choices? 
-        Stop reducing choices by yourself.
-        I mean, we tend to narrow our chioces by the internal internal voice not external conditions. 
+        Stop reducing choices.
+        I mean, we tend to narrow our chioces by the internal internal voice. 
         The voice in our head tells "No, you cannot do that." 
         This is the real enemy. 
         You realizes more choices if you liberate your heart from the noise.
+      </p>
+    <h2>Meditation</h2>
+      <p>
+        Listen to the silence. 
+        Be aware of the sound of nothingness. 
+        Look at the air. 
+        Be aware of the color of emptiness. 
+        Travel here. 
+        Be the present.
       </p>
     </div>
   );
