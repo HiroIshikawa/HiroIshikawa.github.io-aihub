618e2881f388bbb849724eda1f7187f81fa55d10
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-24 20:12:33 -0800

    Update blog

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -7,8 +7,18 @@ function Blog() {
 
   return (
     <div>
+    <p>I put the most recent blog entry here. I put archives on Medium.</p>
+    <h2>Dig Deeply</h2>
       <p>
-        Blog Page.
+      	We tend to move around, dig shallow, and find nothing valualbe.
+      	From no matter where, when digging deeply enough, you'll find valuable thing.
+      	Truly valuable things are huge enough to be found from many direction.
+      	It is the core. It locates at very deep spot.
+      	<br></br>
+      	Why do we give up to dig deeply enough?
+      	Because of fear of missing out.
+      	How can we eliminate the fear of missing out?
+      	Just keep digging. Do not get back to the surface.
       </p>
     </div>
   );
