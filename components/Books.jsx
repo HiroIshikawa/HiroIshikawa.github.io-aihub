8d1e3c0d16565f336ece2e7df3a63744552d1d61
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-27 01:56:23 -0800

    Revert books page link

--- components/Books.jsx
+++ components/Books.jsx
@@ -15,16 +15,18 @@ function Books() {
         <ul>
         	<li><a href={sovereignLink} target="_blank">The Sovereign Individual — Mastering the Transition to the Information Age by James Dale Davidson, William Ress-Mogg </a>
         	helps you to thrive in in 21st century.</li>
-          <li><a href={zeroToOneLink} target="_blank">Zero to One: Notes on Startups, or How to Build the Future by Peter Thiel, Blake Masters </a>
-          helps you to be right while others are wrong.</li>
-          <li><a href={sixEasyPiecesLink} target="_blank">Six Easy Pieces: Essentials of Physics Explained by Its Most Brilliant Teacher by Richard P. Feynman </a>
-          helps you to have a scientist's mindset and know foundation of physics.</li>
-          <li><a href={meditationsLink} target="_blank">Meditations by Marcus Aurelius </a>
-          helps you to be a better version of yourself.</li>
         </ul>
       </p>
     </div>
   );
 }
+
+
+//          <li><a href={zeroToOneLink} target="_blank">Zero to One: Notes on Startups, or How to Build the Future by Peter Thiel, Blake Masters </a>
+  //        helps you to be right while others are wrong.</li>
+     //     <li><a href={sixEasyPiecesLink} target="_blank">Six Easy Pieces: Essentials of Physics Explained by Its Most Brilliant Teacher by Richard P. Feynman </a>
+    //      helps you to have a scientist's mindset and know foundation of physics.</li>
+       //   <li><a href={meditationsLink} target="_blank">Meditations by Marcus Aurelius </a>
+         // helps you to be a better version of yourself.</li>
 
 export default Books;
