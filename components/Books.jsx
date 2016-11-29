55a7a63b1270f51fc37e38dfcedcaaa3b6222dc3
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-28 18:26:56 -0800

    Add book in books

--- components/Books.jsx
+++ components/Books.jsx
@@ -8,19 +8,22 @@ function Books() {
   const zeroToOneLink = 'https://www.amazon.com/Zero-One-Notes-Startups-Future/dp/0804139296';
   const sixEasyPiecesLink = 'https://www.amazon.com/Six-Easy-Pieces-Essentials-Explained/dp/0465025277';
   const meditationsLink = 'https://www.amazon.com/Meditations-Penguin-Great-Marcus-Aurelius-ebook/dp/B00C9SRN5Q/ref=mt_kindle?_encoding=UTF8&me=';
+  const soGoodLink = 'https://www.amazon.com/Good-They-Cant-Ignore-You-ebook/dp/B0076DDBJ6/ref=cm_cr_arp_d_product_top?ie=UTF8';
 
   return (
     <div>
       <p>
         <ul>
+          <li><a href={soGoodLink} target="_blank">So Good They Cant Ignore You: Why Skills Trump Passion in the Quest for Work You Love. </a> 
+          helps you to let passion follow your real work.</li>
+          <li><a href={meditationsLink} target="_blank">Meditations by Marcus Aurelius </a> 
+          helps you to be a better version of yourself.</li>
         	<li><a href={sovereignLink} target="_blank">The Sovereign Individual — Mastering the Transition to the Information Age by James Dale Davidson, William Ress-Mogg </a>
         	helps you to thrive in in 21st century.</li>
           <li><a href={zeroToOneLink} target="_blank">Zero to One: Notes on Startups, or How to Build the Future by Peter Thiel, Blake Masters </a>
           helps you to be right while others are wrong.</li>
           <li><a href={sixEasyPiecesLink} target="_blank">Six Easy Pieces: Essentials of Physics Explained by Its Most Brilliant Teacher by Richard P. Feynman </a>
           helps you to have a scientist's mindset and know foundation of physics.</li>
-          <li><a href={meditationsLink} target="_blank">Meditations by Marcus Aurelius </a> 
-          helps you to be a better version of yourself.</li>
         </ul>
       </p>
     </div>
