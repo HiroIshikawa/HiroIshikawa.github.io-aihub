814833518edab3fdcaff363cbda88fa370c08f64
HiroIshikawa <hiro52ishikawa@gmail.com>
2017-01-09 14:34:47 -0800

    Add date on title and delete previous entry in blog page

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -10,13 +10,8 @@ function Blog() {
     <p>I put the most recent blog entry here. I put archives on Medium.</p>
     <br></br>
     
-    <h2></h2>
-    <h1></h1>
-    <p>
-      
-    </p>
-
     <h2>Reflection</h2>
+    <h1>Jan 9, 2017</h1>
     Accept the currents. It is the nature of the universe. You are the part of it and only daily works can impact to it positively. How much you concerns and think about it, it never change. Yes, it is the reality. It happens and is happening. However, that’s just it. Do not get caught by it. You just do what you do. That’s it. All the things happening in your life does not happen for you or against you. It does just happen as it is. It does has no purpose at all. It just happens. When you accept it, how things work is how you act upon it. Your perception and recognition on it itself becomes how it is for you. But again, it just happens as it is and true form of it is completely independent from it. 
     <br></br>
     <br></br>
@@ -29,51 +24,13 @@ function Blog() {
     When you understand this, you are just a part of changing agent. What you do is part of the system and that’s what you are. Your actions flows into the universe and just happens in the dimension. No more or less. There is no absolute value of that. You only perceive the measure of it and understand that every person has totally different assumptions on it. The nature of the universe is the oneness but the perception of the universe from different people is different. This is the tricky part when you try to consolidate things.
     <br></br>
     <br></br>
-    <br></br>
-    <h2>Depth, Choice, Meditation</h2>
-    <h1>Depth</h1>
-    <p>
-    	We move around, dig shallow, and find nothing valualbe. <br></br>
-    	From no matter where, when digging deeply enough, <br></br>
-      you'll find something valuable. <br></br><br></br>
-    	Valuable things are huge enough to be found from many directions. <br></br>
-    	It is the core. It locates at the very deep spot. <br></br><br></br>
-    	Keep digging. <br></br><br></br>
-    	No matter what happens, keep digging. <br></br>
-    	Don't make any excuse to stop. <br></br><br></br>
-    	No matter what people say, keep digging. <br></br>
-    	Don't give any moment to argue. <br></br><br></br>
-    	Keep digging.
-    </p>
-    <br></br>
-    <h1>Choice</h1>
-    <p>
-      The more choice we have, the happier we feel. <br></br>
-      How can we have more choice? <br></br><br></br>
-      Stop giving up a choice. <br></br><br></br>
-      I mean, we tend to narrow our choice by the internal voice, <br></br>
-      "No, you cannot do that." <br></br><br></br>
-      We don't realize that becasue it's inside us.<br></br><br></br>
-      You gain more choice if you liberate your heart from the noise. <br></br><br></br>
-      Stop giving up a choice. <br></br><br></br>
-      Start making more choice.
-    </p>
-    <br></br>
-    <h1>Meditation</h1>
-      <p>
-        Listen to the silence. <br></br>
-        Be aware of the sound of nothingness. <br></br><br></br>
-        Look at the air. <br></br>
-        Be aware of the color of emptiness. <br></br><br></br>
-        Stand here. <br></br>
-        Be aware of the force of gravity. <br></br><br></br>
-        Take a few minute. <br></br>
-        Be aware of the value of the present. <br></br><br></br>
-        Take one more minute. <br></br><br></br>
-        Now, you go.
-      </p>
     </div>
   );
 }
+
+//<h2></h2>
+//    <h1></h1>
+//    <p>    
+//    </p>
 
 export default Blog;
