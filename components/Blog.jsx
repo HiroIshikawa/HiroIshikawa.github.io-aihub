f120ca842f155b09ce883967f59fd99ef33fea4a
HiroIshikawa <hiro52ishikawa@gmail.com>
2017-01-09 14:28:32 -0800

    Fix spacing in blog entry

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -19,11 +19,15 @@ function Blog() {
     <h2>Reflection</h2>
     Accept the currents. It is the nature of the universe. You are the part of it and only daily works can impact to it positively. How much you concerns and think about it, it never change. Yes, it is the reality. It happens and is happening. However, that’s just it. Do not get caught by it. You just do what you do. That’s it. All the things happening in your life does not happen for you or against you. It does just happen as it is. It does has no purpose at all. It just happens. When you accept it, how things work is how you act upon it. Your perception and recognition on it itself becomes how it is for you. But again, it just happens as it is and true form of it is completely independent from it. 
     <br></br>
+    <br></br>
     The thing is that it did not cause you stop working on it. It just happened to miss the currents of planning and execution. The illusion is that the perception that bad events and things stop you from doing what you do. No, it does not. What’s just happening is what you do. Ultimately, you are what you do. If you would like to keep doing what you do, you do plan and execute constantly. Accept the fact that all the illusion you see is just that’s it.
+    <br></br>
     <br></br>
     Alright, all the suffering are also ego. I think the sensation that you are suffering is ego itself. If you unleash yourself from ego, you can just focus on what you do. If you are feeling like “this is challenging” and “I should be determined to do this things” then it is a trick of ego. Ego will cover the true yourself and courte with brighter inks so it looks different. You will not realize that but if you take it personally it comes from ego. All the sadness, fear, anger come from this notation “you have identity. You are unique. You are different.” This is not true with respect to the nature of the universe. The nature of universe is oneness. The total inclusiveness of every single matters. There is no segregation.
     <br></br>
+    <br></br>
     When you understand this, you are just a part of changing agent. What you do is part of the system and that’s what you are. Your actions flows into the universe and just happens in the dimension. No more or less. There is no absolute value of that. You only perceive the measure of it and understand that every person has totally different assumptions on it. The nature of the universe is the oneness but the perception of the universe from different people is different. This is the tricky part when you try to consolidate things.
+    <br></br>
     <br></br>
     <br></br>
     <h2>Depth, Choice, Meditation</h2>
