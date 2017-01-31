2423aaad0e6d7145ae87b52e730e1a12bdf5dcbc
HiroIshikawa <hiro52ishikawa@gmail.com>
2017-01-31 05:18:11 -0800

    Cleaned

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,46 +2,11 @@ import React from 'react';
 import { Link } from 'react-router';
 
 function Home() {
-  // about me
-  const reactLink = 'https://github.com/facebook/react';
-  const reactCreateLink = 'https://facebook.github.io/react/blog/2016/07/22/create-apps-with-no-configuration.html';
-  const ghPagesLink = 'https://pages.github.com/';
-  const reactCreateGhDeploy = 'https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#github-pages';
-  const myTwLink = 'https://twitter.com/tk2bit';
-  const icsLink = 'https://www.ics.uci.edu/';
-  const dmLink = 'https://drivemode.com/';
-  const dmTopSuLink = 'https://twitter.com/Drivemode/status/801953164446441473';
-  const dmInstaDcLink = 'https://www.instagram.com/p/4m53p0vB7h/?taken-by=drivemodeapp';
-  const mifosPRLink = 'https://github.com/openMF/community-app/pull/1541';
-  const mifosXLink = 'http://mifos.org/mifos-x/';
-  const instaPhotoLink = 'https://github.com/HiroIshikawa/21playground/tree/master/instaphoto';
-  const twentyOneLink = 'https://21.co/';
-  const hxPodLink = 'http://www.hyperxite.com/pod/';
-  const podCompeLink = 'http://www.spacex.com/hyperloop';
-  const uciRobotArmLink = 'https://github.com/HiroIshikawa/object-recognizer';
-  const hxLink = 'http://www.hyperxite.com/';
-  const meLink = 'https://github.com/HiroIshikawa/HiroIshikawa.github.io-me';
-
-  // deploy
   const githubLink = 'https://github.com/HiroIshikawa';
-
-  const mediumLink = 'https://medium.com/@tk2bit';
-
   const linkedInLink = 'https://www.linkedin.com/in/takahiroishikawa';
-
-  const deployBlog = 'https://medium.com/@tk2bit/deploy-d490509408a6#.2rgfu3klq'; 
-
+  const mediumLink = 'https://medium.com/@tk2bit';
+  const myTwLink = 'https://twitter.com/tk2bit';
   const bestBooksLink = 'https://medium.com/@tk2bit/reading-list-2263ec754ab4#.pe2d4ps4h';
-  const readABookEverydayLink = 'https://medium.com/@tk2bit/how-to-read-a-book-everyday-6c02d443011e#.zctt7vpb1';
-
-  // Blog
-  
-  const theDayYouBecomeAGoodWriterLink = 'http://blog.dilbert.com/post/127310496506/the-day-you-became-a-better-writer-2nd-look';
-
-  const grammerMistakeLink = 'http://blog.hubspot.com/marketing/common-grammar-mistakes-list#sm.000006f0ciqynnctuq0frvuzsngng';
-
-  // Note
-
 
   return (
     <div>
@@ -55,12 +20,10 @@ function Home() {
           <br></br>
           Today, Jan 31, 2017, What I am doing:
           <ul>
-            <li>Workig on buliding simple operating system.</li>
+            <li><a href="https://github.com/HiroIshikawa/mini-os-simulator" target="_blank">Workig on buliding simple operating system.</a></li>
             <li><a href="https://github.com/HiroIshikawa/object-recognizer" target="_blank">Implementing robust rotation script.</a></li>
           </ul>
           
-          
-          <br></br> 
           <br></br>
           Coding:
           <a href={githubLink} target="_blank"> @HiroIshikawa</a>
@@ -79,8 +42,8 @@ function Home() {
           <br></br>
           <br></br>
           
-          Living on 
-          <a href={deployBlog} target="_blank"> "Deploy"</a>
+          Practicing
+          <a href="https://medium.com/@tk2bit/deliverable-and-cadence-10595cc4d099#.1clcpk5fu" target="_blank"> "Deliverable and Cadence"</a>
 
           <br></br>
           <br></br>
